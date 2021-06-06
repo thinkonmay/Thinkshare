33fcb0dba0568e9290202692a54245c4f1071ac1
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-06-06 11:05:33 +0700

    add host project

--- Agent/Framework.h
+++ Agent/Framework.h
@@ -1,1 +1,11 @@
-#pragma once
+#pragma once 
+#include <Windows.h>
+#include <iostream>
+
+#include <string.h>
+
+#include <libsoup\soup.h>
+#include <libsoup\soup-websocket.h>
+#include <glib-2.0\glib.h>
+#include <libsoup\soup-types.h>
+#include <json-glib\json-glib.h>
