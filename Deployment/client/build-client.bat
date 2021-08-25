af99fc115eb453caf54cd19ddcc41aab64f7f45f
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-08-25 13:18:39 +0700

    change ip from localhost to viettelIP

--- Deployment/client/build-client.bat
+++ Deployment/client/build-client.bat
@@ -2,8 +2,8 @@ cd ../../Client/services_page
 build.bat
 
 
-@REM docker build . -t 192.168.1.6:5000/flutter:flutter_v1.0.0
-@REM docker push 192.168.1.6:5000/flutter:flutter_v1.0.0
+@REM docker build . -t 125.212.237.45:5000/flutter:flutter_v1.0.0
+@REM docker push 125.212.237.45:5000/flutter:flutter_v1.0.0
 
 
 
