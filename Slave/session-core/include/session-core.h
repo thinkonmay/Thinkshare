f93468507aff5c2c555e10830120db31abd6b516
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-09-08 23:16:32 +0700

    update landing page

--- Slave/session-core/include/session-core.h
+++ Slave/session-core/include/session-core.h
@@ -64,19 +64,59 @@ void			session_core_finalize					(SessionCore* self,
 														gint exit_code,
 														GError* error);
 
+/// <summary>
+/// get pipeline from session core object
+/// </summary>
+/// <param name="self"></param>
+/// <returns></returns>
 Pipeline*		session_core_get_pipeline				(SessionCore* self);
 
+/// <summary>
+/// get webrtchub from session core object, 
+/// webrtc hub will be use for data transfer between session core and client 
+/// through webrtc datachannel 
+/// </summary>
+/// <param name="self"></param>
+/// <returns></returns>
 WebRTCHub*		session_core_get_rtc_hub				(SessionCore* self);
 
+/// <summary>
+/// get quality of experience from session core object 
+/// </summary>
+/// <param name="self"></param>
+/// <returns></returns>
 QoE*			session_core_get_qoe					(SessionCore* self);
 
+/// <summary>
+/// set state of session core object,
+/// </summary>
+/// <param name="core">a string represent state of session core</param>
+/// <param name="state"></param>
 void			session_core_set_state					(SessionCore* core, 
 														 CoreState state);
 
+/// <summary>
+/// get core state from session core object
+/// </summary>
+/// <param name="self"></param>
+/// <returns></returns>
 CoreState		session_core_get_state					(SessionCore* self);
 
+/// <summary>
+/// get signalling hub from session core object,
+/// signalling hub will be use to communicate with signalling server
+/// for register remote session and exchange sdp and ice 
+/// </summary>
+/// <param name="core"></param>
+/// <returns></returns>
 SignallingHub*	session_core_get_signalling_hub			(SessionCore* core);
 
+/// <summary>
+/// get ipc (inter process communication object from session core object)
+/// ipc object will be use to exchange data with agent object
+/// </summary>
+/// <param name="core"></param>
+/// <returns></returns>
 IPC*			session_core_get_ipc					(SessionCore* core);
 
 /// <summary>
@@ -87,6 +127,11 @@ IPC*			session_core_get_ipc					(SessionCore* core);
 void			report_session_core_error				(SessionCore* self,
 														 ErrorCode code);
 
+/// <summary>
+/// get main context from session core
+/// </summary>
+/// <param name="core"></param>
+/// <returns></returns>
 GMainContext*	session_core_get_main_context			(SessionCore* core);
 
 #endif 
