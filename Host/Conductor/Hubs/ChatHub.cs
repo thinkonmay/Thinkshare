de330850fc456bf364adc08b17a96d790c4b8455
epitchi <thienvanlea1@gmail.com>
2021-09-10 10:12:49 +0700

    Fix ChatHub Authorize

--- Host/Conductor/Hubs/ChatHub.cs
+++ Host/Conductor/Hubs/ChatHub.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
-    [Authorize]
     public class ChatHub: Hub
     {
         public async Task sendMessage(string user, string message)
