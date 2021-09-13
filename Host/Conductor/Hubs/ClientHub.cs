820eac86e7e5cc5b1202988c0b63c1b1fba1c62a
epitchi <thienvanlea1@gmail.com>
2021-09-13 16:18:17 +0700

    add _admin trigger report remote control reconnect

--- Host/Conductor/Hubs/ClientHub.cs
+++ Host/Conductor/Hubs/ClientHub.cs
@@ -7,37 +7,32 @@ using Conductor.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using SharedHost.Models.Device;
+using Conductor.Interfaces;
+using SharedHost.Models.Session;
 
 namespace SignalRChat.Hubs
 {
     public interface IClientHub
-    {   
-        public async Task TriggerFunction(int slaveId)
-        {
-            await ReportSlaveObtained(slaveId);
-        }
-
+    {
         /// <summary>
         /// When slave device not use by current user, but obtained by someone => trigger and noti for all user connect this hub (ke ca someone).
         /// </summary>
         /// <param name="slaveID"></param>
         /// <returns></returns>
         Task ReportSlaveObtained(int slaveID);
-        
+
         /// <summary>
         /// Slave device end session with another user => trigger this func => noti new slave available for all users
         /// </summary>
         /// <param name="device"></param>
         /// <returns></returns>
         Task ReportNewSlaveAvailable(SlaveDeviceInformation device);
-
         /// <summary>
         /// Disconnected by something wrong on server => report to user use this device
         /// </summary>
         /// <param name="slaveID"></param>
         /// <returns></returns>
         Task ReportSessionDisconnected(int slaveID);
-
         /// <summary>
         /// Else behind
         /// </summary>
@@ -45,12 +40,23 @@ namespace SignalRChat.Hubs
         /// <returns></returns>
         Task ReportSessionReconnected(int slaveID);
     }
-    
+
     [Authorize]
     public class ClientHub : Hub<IClientHub>
     {
-        public async Task CheckTrigger(int slaveId)
+        private readonly IAdmin _admin;
+        public ClientHub(IAdmin admin)
         {
+            _admin = admin;
+        }
+        public void trigger(string message)
+        {
+            var remotesession = new RemoteSession()
+            {
+                ClientID = 1,
+                SlaveID = 2
+            };
+            _admin.ReportRemoteControlReconnect(remotesession);
         }
     }
 }
