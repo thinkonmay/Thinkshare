530d5003d6abc0f09859749880ca3aafb98aa414
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-09-15 22:59:49 +0700

    add migration to repo

---
+++ Host/Conductor/Migrations/20210914093005_delete relation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Conductor.Migrations
+{
+    public partial class deleterelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeneralError");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GeneralError",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true),
+                    ErrorTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    MachineID = table.Column<int>(type: "integer", nullable: true),
+                    Module = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneralError", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GeneralError_Devices_MachineID",
+                        column: x => x.MachineID,
+                        principalTable: "Devices",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneralError_MachineID",
+                table: "GeneralError",
+                column: "MachineID");
+        }
+    }
+}
---
+++ Host/Conductor/Migrations/20210914102704_update-user.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Conductor.Migrations
+{
+    public partial class updateuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Jobs",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Jobs",
+                table: "AspNetUsers");
+        }
+    }
+}
