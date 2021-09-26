8b6ae2bea1a53841bdbbd653a994dbc49a0575a0
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-09-26 14:00:22 +0700

    🎉 initialize shellextractor server

---
+++ Host/ShellExtractor/Controllers/SlaveMetricController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShellExtractor.Controllers
+{
+    [ApiController]
+    [Route("/SlaveMetric")]
+    [Produces("application/json")]
+    public class SlaveMetricController : ControllerBase
+    {
+
+        private readonly ILogger<SlaveMetricController> _logger;
+
+        public SlaveMetricController(ILogger<SlaveMetricController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("CPU")]
+        public async Task<IActionResult> CPUMetric()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("GPU")]
+        public async Task<IActionResult> GPUMetric()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("RAM")]
+        public async Task<IActionResult> RAMMetric()
+        {
+
+            return Ok();
+        }
+
+        [HttpGet("Storage")]
+        public async Task<IActionResult> StorageMetric()
+        {
+
+            return Ok();
+        }
+
+    }
+}
