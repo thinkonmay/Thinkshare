e28ecbeac77952bf5fc6f80df90925e4b729489e
pigeatgarlic <huyhoangdo0205@gmail.com>
2021-10-14 11:06:29 +0700

    change deployment method

--- Deployment/kubernetes/auto-deploy.sh
+++ Deployment/kubernetes/auto-deploy.sh
@@ -1,28 +1,29 @@
+kubectl apply -f ./logging/namespace.yaml
+kubectl apply -f ./volume/volume-class.yaml
+kubectl apply -f ./volume/volume.yaml
+kubectl apply -f ./volume/volume-claim.yaml
 
-sudo kubectl apply -f ./deployment/systemdb.yaml
+kubectl apply -f ./deployment/systemdb.yaml
 
-sudo kubectl apply -f ./configmap/dbmanagerconfig.yaml
-sudo kubectl apply -f ./configmap/dbconfig.yaml
+kubectl apply -f ./configmap/dbmanagerconfig.yaml
+kubectl apply -f ./configmap/dbconfig.yaml
 
-sudo kubectl apply -f ./ingress/cert-manager/issuer.yaml
-sudo kubectl apply -f ./ingress/ingress.yaml
-
-sudo kubectl apply -f ./volume/volume-class.yaml
-sudo kubectl apply -f ./volume/volume.yaml
-sudo kubectl apply -f ./volume/volume-claim.yaml
+kubectl apply -f ./ingress/ingress.yaml
 
 
-sudo kubectl apply -f ./deployment/dbmanager.yaml
-sudo kubectl apply -f ./deployment/conductor.yaml
-sudo kubectl apply -f ./deployment/resources.yaml
-sudo kubectl apply -f ./deployment/signalling.yaml
-sudo kubectl apply -f ./deployment/slavemanager.yaml
-sudo kubectl apply -f ./deployment/document.yaml
 
-sudo kubectl apply -f ./logging/namespace.yaml
-sudo kubectl apply -f ./logging/elasticsearch.yaml
-sudo kubectl apply -f ./logging/elastic_svc.yaml
-sudo kubectl apply -f ./logging/fluentd.yaml
-sudo kubectl apply -f ./logging/ingress.yaml
-sudo kubectl apply -f ./logging/kibana.yaml
+kubectl apply -f ./deployment/dbmanager.yaml
+kubectl apply -f ./deployment/conductor.yaml
+kubectl apply -f ./deployment/signalling.yaml
+kubectl apply -f ./deployment/slavemanager.yaml
+kubectl apply -f ./deployment/document.yaml
+kubectl apply -f ./deployment/landing-page.yaml
+kubectl apply -f ./deployment/service-page.yaml
+kubectl apply -f ./deployment/admin-page.yaml
+
+kubectl apply -f ./logging/elasticsearch.yaml
+kubectl apply -f ./logging/elastic_svc.yaml
+kubectl apply -f ./logging/fluentd.yaml
+kubectl apply -f ./logging/ingress.yaml
+kubectl apply -f ./logging/kibana.yaml
 
