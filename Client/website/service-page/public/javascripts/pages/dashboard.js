3cacb84aed704aa6686aa0a2e23dc582d4694996
epitchi <thienvanlea1@gmail.com>
2021-10-18 10:13:28 +0700

    Add Detect Device

--- Client/website/service-page/public/javascripts/pages/dashboard.js
+++ Client/website/service-page/public/javascripts/pages/dashboard.js
@@ -51,6 +51,11 @@ $(document).ready(async () => {
 	search()
 	inbox()
 	user()
+	if(isElectron()){
+		// For Electron App
+	} else {
+		// For Web
+	}
 
 	try {
 		const userinfor = await (await API.getInfor()).json()
@@ -237,6 +242,25 @@ function user() {
 	document.getElementById('userButton').addEventListener('click', function () {
 		Utils.responseError("Error!!!", "This feature hasn't developed! \n Next version will be update.", "info")
 	});
+}
+
+function isElectron() {
+	// Renderer process
+	if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+		return true;
+	}
+
+	// Main process
+	if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+		return true;
+	}
+
+	// Detect the user agent when the `nodeIntegration` option is set to true
+	if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+		return true;
+	}
+
+	return false;
 }
 
 function setDataForChart(){
