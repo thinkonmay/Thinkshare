e7209f1a1bbe94b2bdd47db90529757d62922021
epitchi <thienvanlea1@gmail.com>
2021-10-19 09:11:23 +0700

    Optimize electron app

--- Client/native-app/electron/main.js
+++ Client/native-app/electron/main.js
@@ -1,23 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const child_process = require('child_process');
-function createWindow() {
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
     show: false,
     icon: 'assets/logo.ico',
   });
+  mainWindow.setTitle(require('./package.json').name);
   mainWindow.maximize();
   mainWindow.show();
   mainWindow.loadURL("https://service.thinkmay.net/")
-}
+};
 
-app.whenReady().then(() => {
-  createWindow()
-})
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
 
-
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.  
 
