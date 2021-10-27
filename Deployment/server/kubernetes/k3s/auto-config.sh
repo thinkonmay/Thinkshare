1f9a8411eb5c82f2816c0b18d9b8eb1bad99451a
Ubuntu <ubuntu@ip-172-31-12-136.ap-southeast-1.compute.internal>
2021-10-27 09:18:28 +0000

    add cert manager instruction

--- Deployment/server/kubernetes/k3s/auto-config.sh
+++ Deployment/server/kubernetes/k3s/auto-config.sh
@@ -20,3 +20,4 @@ curl -sfL https://get.k3s.io | sh -
 
 k3s kubectl get node
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
+snap install helm --classic
