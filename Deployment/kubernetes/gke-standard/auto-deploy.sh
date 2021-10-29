a7bdcc69d4020c38997e9ca7f96a9d600f68071b
pigeatgarlic <huyhoangdo0205@gmail.com>
2021-10-29 10:24:13 +0700

    add gke support

--- Deployment/kubernetes/gke-standard/auto-deploy.sh
+++ Deployment/kubernetes/gke-standard/auto-deploy.sh
@@ -1,9 +1,4 @@
-kubectl apply -f ./ingress/ingress.yaml
-kubectl apply -f ./deployment/dbmanager.yaml
-kubectl apply -f ./deployment/conductor.yaml
-kubectl apply -f ./deployment/signalling.yaml
-kubectl apply -f ./deployment/slavemanager.yaml
-kubectl apply -f ./deployment/document.yaml
-kubectl apply -f ./deployment/landing-page.yaml
-kubectl apply -f ./deployment/service-page.yaml
-kubectl apply -f ./deployment/admin-page.yaml
+for file in ./*
+do
+  kubectl apply -f "$file"
+done
