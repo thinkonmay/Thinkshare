c9ad44695a4d173f305ed8bdadfdcac7ea42a32c
pigeatgarlic <huyhoangdo0205@gmail.com>
2021-10-29 09:50:14 +0700

    add terraform

--- Deployment/server/kubernetes/gke-standard/auto-deploy.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f ./ingress/ingress.yaml
-kubectl apply -f ./deployment/conductor.yaml
-kubectl apply -f ./deployment/signalling.yaml
-kubectl apply -f ./deployment/slavemanager.yaml
-kubectl apply -f ./deployment/landing-page.yaml
-kubectl apply -f ./deployment/service-page.yaml
-kubectl apply -f ./deployment/admin-page.yaml
