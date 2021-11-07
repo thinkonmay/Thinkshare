11e239c20c765fc12738c7b00b323baa353ef1f7
pigeatgarlic <huyhoangdo0205@gmail.com>
2021-11-07 11:05:03 +0700

    add file

---
+++ Host/SharedHost/Models/Hub/EventModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedHost.Models.Hub
+{
+    public class EventModel
+    {
+        public string EventName {  get; set; }
+
+        public string Message { get; set; }
+    }
+}
