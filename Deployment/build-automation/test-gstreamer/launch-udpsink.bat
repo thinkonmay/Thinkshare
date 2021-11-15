d87173626a026bf9f93a559995e3e531cff03047
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-11-15 10:51:06 +0700

    ➗ devide repo

--- Deployment/build-automation/test-gstreamer/launch-udpsink.bat
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc uri=udp://localhost:5430 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H265 ! queue ! decodebin ! queue  ! d3dvideosink create-render-window=true
--- Slave/shared-items/include/key-convert.h
+++
@@ -1,4 +0,0 @@
-#include <glib.h>
-
-unsigned short
-convert_javascript_key_to_window_key(gchar* keycode);
