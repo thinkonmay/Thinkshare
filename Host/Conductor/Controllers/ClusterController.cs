30990201458f89c35a5afe73f30da27c8f50055d
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-11-23 19:33:50 +0700

    add file

---
+++ Host/Conductor/Controllers/ClusterController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DbSchema.SystemDb.Data;
+using Conductor.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SharedHost.Models.User;
+using SharedHost.Models.Device;
+using SharedHost.Auth.ThinkmayAuthProtocol;
+using SharedHost.Models.Cluster;
+using SharedHost;
+using RestSharp;
+
+namespace Conductor.Controllers
+{
+    /// <summary>
+    /// Routes used by user to fetch information about the system
+    /// </summary>
+    [Manager]
+    [ApiController]
+    [Route("/Cluster")]
+    [Produces("application/json")]
+    public class ClusterController : Controller
+    {
+        private readonly UserManager<UserAccount> _userManager;
+
+        private readonly ApplicationDbContext _db;
+
+        private readonly IWorkerCommnader _slmsocket;
+
+        private readonly RestClient _restClient;
+
+        public ClusterController(ApplicationDbContext db,
+                            UserManager<UserAccount> userManager,
+                            IWorkerCommnader slm,
+                            SystemConfig config)
+        {
+            _slmsocket = slm;
+            _db = db;
+            _userManager = userManager;
+            _restClient = new RestClient(config.Authenticator + "/Token");
+        }
+
+
+
+
+
+        /// <summary>
+        /// Get list of available slave device, contain device information
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Register")]
+        public async Task<IActionResult> NewCluster(bool Private , string TURN)
+        {
+            var ManagerID = HttpContext.Items["UserID"];
+            UserAccount account =  await _userManager.FindByIdAsync((string)ManagerID);
+            account.ManagedCluster.Add(new GlobalCluster 
+            { 
+                TURN = TURN,
+                Register = DateTime.Now, 
+                Private = Private 
+            });
+            await _userManager.UpdateAsync(account);
+            var updated_cluster = (await _userManager.FindByIdAsync((string)ManagerID)).ManagedCluster.First();
+
+            var request = new RestRequest("GrantCluster")
+                .AddJsonBody(updated_cluster);
+            request.Method = Method.POST;
+
+            var result = await _restClient.ExecuteAsync(request);
+            return Ok(result.Content);
+        }
+
+
+        /// <summary>
+        /// Get list of available slave device, contain device information
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Infor")]
+        public IActionResult GetInfor(int ID)
+        {
+            return Ok(_db.Clusters.Find(ID));
+        }
+    }
+}
---
+++ Host/WorkerManager/Controllers/CoreController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkerManager.Interfaces;
+using System.Threading.Tasks;
+using SharedHost.Models.Session;
+using SharedHost;
+using WorkerManager.Middleware;
+using WorkerManager.Data;
+using SharedHost.Models.Device;
+
+// TODO: authentification
+
+namespace WorkerManager.Controllers
+{
+    [Route("/session")]
+    [ApiController]
+    [Produces("application/json")]
+    public class CoreController : Controller
+    {
+
+        private readonly IWorkerNodePool _slavePool;
+
+        private readonly ClusterDbContext _db;
+
+        public CoreController(SystemConfig config,
+                              IWorkerNodePool slavePool,
+                              ClusterDbContext db)
+        {
+            _db = db;
+            _slavePool = slavePool;
+        }
+
+        [Worker]
+        [HttpPost("token")]
+        public async Task<IActionResult> Session()
+        {
+            var PrivateID = HttpContext.Items["PrivateID"];
+            var Node = _db.Devices.Find(PrivateID);
+            if (Node._workerState == WorkerState.OnSession ||
+               Node._workerState == WorkerState.OffRemote)
+            {
+                return Ok(Node.RemoteToken);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Worker]
+        [HttpPost("signalling")]
+        public async Task<IActionResult> Signalling()
+        {
+            var PrivateID = HttpContext.Items["PrivateID"];
+            var Node = _db.Devices.Find(PrivateID);
+            if (Node._workerState == WorkerState.OnSession ||
+               Node._workerState == WorkerState.OffRemote)
+            {
+                return Ok(Node.RemoteToken);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Worker]
+        [HttpPost("turn")]
+        public async Task<IActionResult> Turn()
+        {
+            var PrivateID = HttpContext.Items["PrivateID"];
+            var Node = _db.Devices.Find(PrivateID);
+            if (Node._workerState == WorkerState.OnSession ||
+               Node._workerState == WorkerState.OffRemote)
+            {
+                return Ok(Node);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Worker]
+        [HttpPost("qoe")]
+        public async Task<IActionResult> QoE()
+        {
+            var PrivateID = HttpContext.Items["PrivateID"];
+            var Node = _db.Devices.Find(PrivateID);
+            if (Node._workerState == WorkerState.OnSession ||
+               Node._workerState == WorkerState.OffRemote)
+            {
+                return Ok(Node.QoE);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
