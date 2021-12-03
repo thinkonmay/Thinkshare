1a77c1c932da1a2613f43ba280f34f0dc61b24a8
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-12-03 09:05:02 +0700

    add file

---
+++ Host/DbSchema/LocalDb/Models/Log.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbSchema.LocalDb.Models
+{
+    public class Log
+    {
+        [Key]
+        public int ID {get;set;}
+
+        public string Content{get;set;}
+
+        public DateTime LogTime {get;set;}
+        
+        public virtual ClusterWorkerNode worker {get;set;}       
+    }
+}
