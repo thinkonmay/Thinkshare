2520ba751e8f54ba7a791e01735f60fd66b85073
epitchi <thienvanlea1@gmail.com>
2021-12-04 10:03:42 +0700

    update policies for register cluster

--- Client/cluster/manager-page/src/policies.js
+++ Client/cluster/manager-page/src/policies.js
@@ -2,5 +2,6 @@ module.exports = {
 	"*": true,
 	"/dashboard": "is-logged-in",
 	"/setting": "is-logged-in",
-	"/initialize": "is-logged-in"
+	"/initialize": "is-logged-in",
+	"/register-cluster": "is-logged-in"
 }
