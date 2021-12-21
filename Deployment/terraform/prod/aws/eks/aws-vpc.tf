1de3b76e9d43e84b762c4596e94cdeefd7888db0
pigeatgarlic <huyhoangdo0205@gmail.com>
2021-12-21 18:53:30 +0700

    Infrastructure: fix volume mount 🛠️

--- Deployment/terraform/prod/aws/eks/aws-vpc.tf
+++ Deployment/terraform/prod/aws/eks/aws-vpc.tf
@@ -27,6 +27,18 @@ resource "aws_security_group" "all_worker_mgmt" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+
+  ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
--- Deployment/terraform/prod/aws/eks/volume.tf
+++ Deployment/terraform/prod/aws/eks/volume.tf
@@ -1,7 +1,8 @@
 
+
 resource "aws_ebs_volume" "caching-block" {
   availability_zone = "ap-southeast-1b"
-  size              = 5
+  size              = 10
   type              = "gp2"
 
   tags = {
@@ -9,90 +10,34 @@ resource "aws_ebs_volume" "caching-block" {
   }
 }
 
-resource "aws_security_group" "efs-security-group" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
+resource "aws_ebs_volume" "database-block" {
+  availability_zone = "ap-southeast-1b"
+  size              = 50
+  type              = "gp2"
 
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-}
-
-
-
-///////////////////////////////////
-resource "aws_efs_file_system" "efs" {
-  creation_token = "eks-volume"
   tags = {
-    Name = "eks-volume"
+    Name = "Caching volume"
   }
 }
-output "efs-logging-id" {
-  description = "id of efs file system"
-  value = "${aws_efs_file_system.efs.id}"
-}
 
-// required for eks volume
-resource "aws_efs_mount_target" "volume" {
-  file_system_id = "${aws_efs_file_system.efs.id}"
+resource "aws_ebs_volume" "logging-block" {
+  availability_zone = "ap-southeast-1b"
+  size              = 50
+  type              = "gp2"
 
-  security_groups = [
-    aws_security_group.efs-security-group.id
-  ]
-  
-  subnet_id = "${module.vpc.public_subnets[0]}"
-}
-
-
-
-//////////////////////////////////////////
-resource "aws_efs_file_system" "database" {
-  creation_token = "database-volume"
   tags = {
-    Name = "eks-volume"
+    Name = "Caching volume"
   }
 }
-output "efs-database-id" {
-  description = "id of efs file system"
-  value = "${aws_efs_file_system.database.id}"
+
+
+output "caching_volume_id" {
+  value = aws_ebs_volume.caching-block.id
 }
-
-resource "aws_efs_mount_target" "database-eks" {
-  file_system_id = "${aws_efs_file_system.database.id}"
-
-  security_groups = [
-    aws_security_group.efs-security-group.id
-  ]
-  
-  subnet_id = "${module.vpc.public_subnets[0]}"
+output "database_volume_id" {
+  value = aws_ebs_volume.database-block.id
 }
-
-
-
-///////////////////////////////////////////
-resource "aws_efs_file_system" "caching" {
-  creation_token = "caching-volume"
-  tags = {
-    Name = "eks-volume"
-  }
-}
-output "efs-caching-id" {
-  description = "id of efs file system"
-  value = "${aws_efs_file_system.caching.id}"
-}
-
-resource "aws_efs_mount_target" "caching-eks" {
-  file_system_id = "${aws_efs_file_system.caching.id}"
-
-  security_groups = [
-    aws_security_group.efs-security-group.id
-  ]
-  
-  subnet_id = "${module.vpc.public_subnets[0]}"
+output "logging_volume_id" {
+  value = aws_ebs_volume.logging-block.id
 }
