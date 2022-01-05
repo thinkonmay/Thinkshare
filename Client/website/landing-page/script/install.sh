6770773805b0ecb5b50f4bc02970bc56c4705469
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2022-01-05 22:09:21 +0700

    update cluster setup script

--- Client/website/landing-page/script/install.sh
+++ Client/website/landing-page/script/install.sh
@@ -7,11 +7,11 @@ apt-get -y install \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get -y update
 apt-get -y install docker-ce docker-ce-cli containerd.io
