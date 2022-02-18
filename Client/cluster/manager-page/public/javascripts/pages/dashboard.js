06e7a36d5e78e664f5fe0cdc16029889ae37430c
epitchi <thienvanlea1@gmail.com>
2022-02-18 11:00:26 +0700

    Update: Feature Access Connect By Time

--- Client/cluster/manager-page/public/javascripts/pages/dashboard.js
+++ Client/cluster/manager-page/public/javascripts/pages/dashboard.js
@@ -3,11 +3,11 @@ import * as Utils from "../util/utils.js"
 import { setCookie, deleteCookie } from "../util/cookie.js"
 import { appendWorkerNode } from "../util/user-row-component.js"
 import { setDataForChart } from "../util/chart.js"
+import { convertDate } from "../util/date-time-convert.js"
 
 
-async function 
-prepare_cluster_infor()
-{
+async function
+	prepare_cluster_infor() {
 	let ClusterInfor = await (await API.getInforClusterRoute()).json();
 	let workerState = await (await API.getWorkerStateRoute()).json();
 
@@ -21,42 +21,40 @@ prepare_cluster_infor()
 	for (let i = 0; i < data.length; i++) {
 		let workerNodeId = data.at(i).id
 		let state = workerState[workerNodeId]
-		appendWorkerNode(data.at(i).os, 
-		                 data.at(i).cpu, 
-						 data.at(i).gpu, 
-						 data.at(i).id, 
-						 data.at(i).raMcapacity, 
-						 data.at(i).register, 
-						 state)
+		appendWorkerNode(data.at(i).os,
+			data.at(i).cpu,
+			data.at(i).gpu,
+			data.at(i).id,
+			data.at(i).raMcapacity,
+			data.at(i).register,
+			state)
 	}
 
 	$('#DeleteCluster').click(() => {
-		Utils.newSwal.fire({ 
-		title: 'Are you sure?',
-		text: "You won't be able to revert this!",
-		icon: 'warning',
-		showCancelButton: true,
-		confirmButtonColor: '#3085d6',
-		cancelButtonColor: '#d33',
-		confirmButtonText: 'Yes, delete it!'
+		Utils.newSwal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!'
 		}).then(async (result) => {
 			if (result.isConfirmed) {
 				var res = await API.UnRegister();
 
-				if(res.ok)
+				if (res.ok)
 					Swal.fire('Success!', 'Cluster deleted.', 'success')
-				else	
+				else
 					Swal.fire('Fail!', 'Cluster deletion failed.', 'error')
-		}})
+			}
+		})
 	});
 }
-
-function
-setup_cluster_booking()
-{
-	let timeStampFrom = 	$("#stamp-time-from").val()
-	let timeStampTo = 		$("#stamp-time-to").val()
-	let bookUser = 			$("#stamp-time-to").val()
+function setup_cluster_booking() {
+	let timeStampFrom = $("#stamp-time-from").val()
+	let timeStampTo = $("#stamp-time-to").val()
+	let bookUser = $("#stamp-time-to").val()
 
 	$("#stamp-time-from").on("change", function () {
 		timeStampFrom = $("#stamp-time-from").val()
@@ -69,16 +67,29 @@ setup_cluster_booking()
 	})
 
 	$('#AddBooking').click(async () => {
-		var res = await API.createPermanentNewRole(bookUser);
+		let startTime = convertDate(timeStampFrom)
+		let endTime = convertDate(timeStampTo)
+		console.log(startTime)
+		console.log(endTime)
+		console.log(bookUser)
+		if (startTime == endTime) {
+			Swal.fire('Fail!', 'startTime and endTime must have the difference', 'error')
+		} else if (startTime > endTime) {
+			Swal.fire('Fail!', 'startTime must be low to endTime', 'error')
+		}
+		else {
+			var res = await API.createNewRole(startTime, endTime, bookUser)
+			if (res.ok)
+				Swal.fire('Success!', `Booking success from ${String(new Date(startTime)).slice(0, 33)} to ${String(new Date(endTime)).slice(0, 33)} for user "${bookUser}".`, 'success')
+			else
+				Swal.fire('Fail!', 'Booking failed. Please check and try again.', 'error')
+		}
 
-		if(res.ok)
-			Swal.fire('Success!', 'Booking success.', 'success')
-		else	
-			Swal.fire('Fail!', 'Booking failed.', 'error')
 	})
 }
 
 $(document).ready(async () => {
+	setCurrentDateTime()
 	setup_cluster_booking();
 	prepare_cluster_infor();
 	set_worker_chart();
@@ -99,9 +110,8 @@ $(document).ready(async () => {
 	})
 })
 
-function 
-set_worker_chart()
-{
+function
+	set_worker_chart() {
 	$('#analyticCPU').click(() => {
 		setDataForChart('#1F3BB3', 'CPU', false);
 	});
@@ -117,17 +127,17 @@ set_worker_chart()
 	})
 }
 
-function setCurrentDateTimeLog() {
-	let currentDate = 	String(new Date().toISOString()).slice(0, 16)
-	let minDate = 		String(new Date((new Date()).getTime() - 86400000).toISOString()).slice(0, 16)
-	let defaultDate = 	String(new Date((new Date()).getTime() - 3600).toISOString()).slice(0, 16)
+function setCurrentDateTime() {
+	let maxDate = String(new Date((new Date()).getTime() +  + 86400000000).toISOString()).slice(0, 16)
+	let minDate = String(new Date((new Date()).getTime()).toISOString()).slice(0, 16)
+	let defaultDate = String(new Date((new Date()).getTime()).toISOString()).slice(0, 16)
 	$('#stamp-time-from').attr({
-		"max": currentDate,
+		"max": maxDate,
 		"min": minDate,
 		"value": defaultDate
 	});
 	$('#stamp-time-to').attr({
-		"max": currentDate,
+		"max": maxDate,
 		"min": minDate,
 		"value": defaultDate
 	});
