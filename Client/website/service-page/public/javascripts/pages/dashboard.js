8460864e24cc97dc607169bf23a4f4017df268e1
epitchi <thienvanlea1@gmail.com>
2022-02-20 12:42:39 +0700

    Move Logout to component

--- Client/website/service-page/public/javascripts/pages/dashboard.js
+++ Client/website/service-page/public/javascripts/pages/dashboard.js
@@ -3,12 +3,11 @@ import { clusterFormGen } from "../util/manager.js"
 import { connectToClientHub } from "../util/clientHub.js"
 import { prepare_setting } from "../util/setting.js"
 import { setDataForChart } from "../util/chart.js"
-import { prepare_user_infor , prepare_worker_dashboard} from "../util/userInfor.js"
+import { prepare_user_infor , prepare_worker_dashboard, prepare_logout} from "../util/userComponent.js"
 import { prepare_user_setting } from "../util/setting-constant.js"
 import { prepare_download_button} from "../util/appVersion.js"
 import { prepare_tutorial_popup } from "../util/popup.js"
 import { logUI } from "../util/api.js"
-
 
 
 $(document).ready(async () => {
@@ -24,18 +23,8 @@ $(document).ready(async () => {
 		prepare_user_setting();
 		prepare_setting();
 		connectToClientHub();
+		prepare_logout();
 	} catch(err){
 		logUI(err.stack)
 	}
-
-	$('#logout').click(() => {
-		setCookie("logout", "true")
-		setCookie("token", null, 1)
-		try {
-			gapi.auth.signOut();
-			window.location = "/login"
-		} catch {
-			window.location = "/login"
-		}
-	})
 })
