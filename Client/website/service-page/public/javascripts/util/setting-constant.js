c75604276d5d151623930f917550df1a8d158c92
epitchi <thienvanlea1@gmail.com>
2022-02-20 09:04:08 +0700

    Fix Fetch Update Account Infor Errors

--- Client/website/service-page/public/javascripts/util/setting-constant.js
+++ Client/website/service-page/public/javascripts/util/setting-constant.js
@@ -255,15 +255,19 @@ prepare_user_setting()
             title: "Updating",
             text: "Wait a minute",
             didOpen: () => {
-                console.log(body)
                 API.updatePassword(updatePassword)
                     .then(async data => {
-                        if (data.status == 200) {
-                            Utils.newSwal.fire({
-                                title: "Success!",
-                                text: "User information has been updated successfully",
-                                icon: "success",
-                            })
+                        const response = await data.json()
+                        if (response.status == 200) {
+                            if (response.errors == null) {
+                                Utils.newSwal.fire({
+                                    title: "Success!",
+                                    text: "User information has been updated successfully",
+                                    icon: "success",
+                                })
+                            } else {
+                                Utils.responseError(response.errors[0].code, response.errors[0].description, "error")
+                            }
                         } else {
                             Utils.responseError("Error!", "The change has failed, check your information and try again.", "error")
                         }
@@ -281,14 +285,20 @@ prepare_user_setting()
                 console.log(body)
                 API.setInfor(body)
                     .then(async data => {
-                        if (data.status == 200) {
+                        const response = await data
+                        if (response.status == 200) {
+                            if (response.errors == null) {
                             body = await (await API.getInfor()).json();
-                            Utils.newSwal.fire({
-                                title: "Success!",
-                                text: "User information has been updated successfully",
-                                icon: "success",
-                            })
-                        } else {
+                                Utils.newSwal.fire({
+                                    title: "Success!",
+                                    text: "User information has been updated successfully",
+                                    icon: "success",
+                                })
+                            } else {
+                                Utils.responseError(response.errors[0].code, response.errors[0].description, "error")
+                            }
+                        }
+                        else {
                             Utils.responseError("Error!", "The change has failed, check your information and try again.", "error")
                         }
                     })
